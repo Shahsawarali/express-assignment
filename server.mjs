aed5f7be64216883a950e7862a823fb5135da8a9
IlOvecompUter1$t <shahva2022@gmail.com>
2024-09-18 14:08:24 +0500

    multiple routes like and 4 HTTP methods get, put, post, delete DONE

--- server.mjs
+++ server.mjs
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 app.use (express.json())
 
+
+// multiple routes
 app.get('/', (req, res) => {
   res.json({Message :'Hello World!'})
 })
@@ -15,22 +17,28 @@ app.get("/contact us", (req, res) => {
     res.json({Message : 'contact us page'})
 })
 
+
+// 4 HTTP methods
+// get request
 app.get('/get', (req, res) => {
     const newItem = req.body
     res.json({Message : 'GET request - Fetching all items',data : newItem})
 })
 
+// post request
 app.post('/', (req, res) => {
     const newItem = req.body
     res.json({Message : 'item added',data : newItem})
 })
 
+// put request 
 app.put('/:Id', (req, res) => {
     const newItem = req.body
     const itemId = req.params.Id
     res.json({Message : `updated item ${itemId}`,data : newItem})
 })
 
+// delete request
 app.delete('/:Id', (req, res) => {
     const itemId = req.params.Id
     res.json({Message : `delete item ${itemId}`})
