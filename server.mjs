8d7200c1a1a758342d0cc2cf43e0166a6c17669c
IlOvecompUter1$t <shahva2022@gmail.com>
2024-09-18 13:09:23 +0500

    defining defining multiple routes

--- server.mjs
+++ server.mjs
@@ -1,9 +1,10 @@
 import express from "express"
 const app = express()
 const port = 3000
+app.use (express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({Message :'Hello World!'})
 })
 
 app.listen(port, () => {
